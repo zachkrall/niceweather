5634d10430db0b718e72ff28860afa3546d3c386
Glitch (niceweather) <none>
2018-12-05 19:42:01 +0000

    🌉🚊 Checkpoint ./server.js:274611/536

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 
 const weather = require('yahoo-weather');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', ( req, res ) => res.send('No') );
+
 app.get('/:location', function(request, response) {
 
   let answer = new Object();
-  
-  // answer.request_name = request.params.location;
   
   weather( request.params.location, 'F' ).then(
   
@@ -37,7 +29,7 @@ app.get('/:location', function(request, response) {
 
 });
 
-// listen for requests :)
+
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
